033c280888adbffc1349752bb67de398518ba465
Juan <braianher2016@gmail.com>
2022-06-20 16:27:23 -0300

    first commit

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+return parseInt(num, 2);
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca  
+return num.toString(2);
+}
+
+//otra forma de resolverlo:
+// function DecimalABinario(num){
+//                  let almacenador = []
+//                     while (num !== 0){
+//                         var division = Math.floor(num / 2)
+//                         var modulo = num % 2
+//                         num = division;
+//                         almacenador.unshift(modulo);
+//                                      }
+//            return almacenador.join("");
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
