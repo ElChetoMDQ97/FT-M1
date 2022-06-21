e6ba145e3478064e130879a5133c05d92c71a691
Juan <braianher2016@gmail.com>
2022-06-21 15:58:49 -0300

    revision1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,9 +1,18 @@
 'use strict'
 
+// digito * base(2) ** posicion
 function BinarioADecimal(num) {
   // tu codigo aca
 return parseInt(num, 2);
 }
+
+//  var res = 0;
+//  var pos = 0;
+// for(let i = num.length-1; i >= 0; i--){
+//num[i]2**pos
+//pos++
+//}
+
 
 function DecimalABinario(num) {
   // tu codigo aca  
@@ -12,15 +21,12 @@ return num.toString(2);
 
 //otra forma de resolverlo:
 // function DecimalABinario(num){
-//                  let almacenador = []
+//                  lvar res = "";
 //                     while (num !== 0){
-//                         var division = Math.floor(num / 2)
-//                         var modulo = num % 2
-//                         num = division;
-//                         almacenador.unshift(modulo);
-//  [1,1,1] === "111"
+//                         res = num % 2 + res;
+//                         num = Math.floor(num / 2);
 //                                      }
-//            return almacenador.join("");
+//            return almacenador;
 
 
 module.exports = {
